85df88bcc28bdffaba8b4b34ef4197d406d06a8f
atsushi_a <eszxc.sc18@gmail.com>
2016-03-13 03:03:47 +0000

    Add account activations

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,7 +22,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -41,9 +41,9 @@ module SessionsHelper
   end
   
   # 現在ログインしているユーザーを返す (ユーザーがログイン中の場合のみ)
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
+  #def current_user
+  #  @current_user ||= User.find_by(id: session[:user_id])
+  #end
   
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
